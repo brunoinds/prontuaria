34413a641004e48815652df1411180b94bb4d312
Bruno Freire <brunoqm99@gmail.com>
2026-04-06 08:15:48 -0300

    Update ServicePrescriptions.ts to correct spelling of 'BUSCOPAN' and add multiple new prescriptions

--- src/data/prescriptions/ServicePrescriptions.ts
+++ src/data/prescriptions/ServicePrescriptions.ts
@@ -12,9 +12,73 @@ const servicePrescriptions = [
         dosis: '1'
     },
     {
-        name: 'BUSCOPAM',
+        name: 'BUSCOPAN',
         unit: 'AMP',
         dosis: '1'
     },
-    
+    {
+        name: 'BUSCOPAN COMPOSTO',
+        unit: 'AMP',
+        dosis: '1'
+    },
+    {
+        name: 'DICLOFENACO',
+        unit: 'AMP',
+        dosis: '1'
+    },
+    {
+        name: 'CETOPROFENO',
+        unit: 'AMP',
+        dosis: '1'
+    },
+    {
+        name: 'DEXAMETASONA',
+        unit: 'AMP',
+        dosis: '1'
+    },
+    {
+        name: 'HIDROCORTISONA',
+        unit: 'MG',
+        dosis: '100'
+    },
+    {
+        name: 'ONDANSETRONA',
+        unit: 'AMP',
+        dosis: '1'
+    },
+    {
+        name: 'DIMENIDRINATO',
+        unit: 'AMP',
+        dosis: '1'
+    },
+    {
+        name: 'METOCLOPRAMIDA',
+        unit: 'AMP',
+        dosis: '1'
+    },
+    {
+        name: 'OMEPRAZOL',
+        unit: 'AMP',
+        dosis: '1'
+    },
+    {
+        name: 'FUROSEMIDA',
+        unit: 'AMP',
+        dosis: '1'
+    },
+    {
+        name: 'PENICILINA G BENZATINA',
+        unit: 'mUI',
+        dosis: '1.2M'
+    },
+    {
+        name: 'TRAMAL',
+        unit: 'MG',
+        dosis: '100'
+    },
+    {
+        name: 'SALBUTAMOL (3 CICLOS)',
+        unit: 'PUFF',
+        dosis: '3'
+    },
 ]
